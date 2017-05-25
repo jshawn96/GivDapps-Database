da27f0260a0b7f7c571fe1445fe357575a00a4c4
lprescott <lukep@nycap.rr.com>
2017-05-25 15:10:12 -0400

    Default Settings

--- GivDapps/GivDapps/__init__.py
+++ GivDapps/GivDapps/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
-from flask.ext.script import Manager
+from flask_sqlalchemy import sqlalchemy
+from flask_migrate import migrate, MigrateCommand
+from flask_script import Manager
 
 app = Flask(__name__)
+app.config.from_object('GivDapps.default_settings')
 manager = Manager(app)
+
+db = sqlalchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+from GivDapps.models import *
 
 @app.route("/")
 def hello():
