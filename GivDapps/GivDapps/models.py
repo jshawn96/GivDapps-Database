7d5c4297a518d65bc99189953e311d8905a8afec
lprescott <lukep@nycap.rr.com>
2017-05-31 22:05:56 -0400

    Added comments; deleted migrations.

--- GivDapps/GivDapps/models.py
+++ GivDapps/GivDapps/models.py
@@ -3,8 +3,9 @@ from sqlalchemy.sql import func
 import datetime
 import cloudinary.utils
 
-#Useful ref: http://flask-sqlalchemy.pocoo.org/2.1/models/
-#More ref: http://docs.sqlalchemy.org/en/latest/orm/basic_relationships.html
+#CodeUpStartTutorial: codeupstart.com/project/build-kickstarter-with-python-and-flask
+#Useful ref: http://flask-sqlalchemy.pocoo.org/2.1/models/ ***
+#It is important to note that this code is using flask AND SQLAlchemy and not just SQLAlchemy.
 
 #Helper Tables for Many-to-Many Relationships
 #This helper table shows all the supporting users for a challenge
