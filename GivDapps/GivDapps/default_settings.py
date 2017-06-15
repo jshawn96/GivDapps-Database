8dae465431275fb13be2cc81ef32c2050d0279fc
jshawn96 <111jasonhall@gmail.com>
2017-06-15 10:38:30 -0400

    Create default_settings.py
    For  user login/signup

--- GivDapps/GivDapps/default_settings.py
+++ GivDapps/GivDapps/default_settings.py
@@ -2,3 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + BASE_DIR + "/app.db"
+
+SECURITY_REGISTERABLE = False
+SECURITY_SEND_REGISTER_EMAIL = False
+SECURITY_EMAIL_SENDER = "GivDapps"
